68a0723c235b54f13dc66e39e7ed5c0f42a8e8cd
Kang <kangwenbo422@icloud.com>
2023-02-07 15:32:31 +0800

    初始化前端代码文件

---
+++ movie-web/src/App.vue
@@ -0,0 +1,13 @@
+<script>
+import { def } from '_@vue_shared@3.2.47@@vue/shared';
+
+export default {}
+</script>
+
+<template>
+  <RouterView />
+</template>
+
+<style scoped lang="scss">
+
+</style>
---
+++ movie-web/vite.config.js
@@ -0,0 +1,24 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue()],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+  // 反向代理
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://192.168.70.99:8080/',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    }
+  }
+})
